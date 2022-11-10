e3bfd796e975669f5b78c5c5e241fe11d899bd7a
Sergey Aleshchenko <serg.aleshchenko@gmail.com>
2022-11-10 20:52:31 +0400

    Another algorithm

--- src/main/java/org/example/WaterCalculator.java
+++ src/main/java/org/example/WaterCalculator.java
@@ -1,35 +1,6 @@
 package org.example;
 
 public class WaterCalculator {
-
-    public long calculateWaterAmount(int[] landscape) {
-        if (!validateCorrectness(landscape)) {
-            return 0;
-        }
-
-        int answer = 0;
-        int size = landscape.length;
-
-        int[] leftMax = new int[size];
-        int[] rightMax = new int[size];
-
-        leftMax[0] = landscape[0];
-        for (int column = 1; column < size; column++) {
-            leftMax[column] = Math.max(landscape[column], leftMax[column - 1]);
-        }
-
-        rightMax[size - 1] = landscape[size - 1];
-        for (int column = size - 2; column >= 0; column--) {
-            rightMax[column] = Math.max(landscape[column], rightMax[column + 1]);
-        }
-
-        for (int column = 1; column < size - 1; column++) {
-            answer += Math.min(leftMax[column], rightMax[column]) - landscape[column];
-        }
-
-        return answer;
-    }
-
     private boolean validateCorrectness(int[] landscape) {
         if (landscape.length == 0) {
             return false;
@@ -40,10 +11,72 @@ public class WaterCalculator {
                 return false;
             }
         }
-
         return true;
     }
 
+    // O(max(maxHeight, N)), O(1)
+    public long calculateWaterAmount(int[] landscape) {
+        if (!validateCorrectness(landscape)) {
+            return 0;
+        }
+
+        int numberOfBlocks = 0;
+        int maxHeight = Integer.MIN_VALUE;
+
+        for (int column = 0; column < landscape.length; column++) {
+            maxHeight = Integer.max(maxHeight, landscape[column]);
+            numberOfBlocks += landscape[column];
+        }
+
+        int totalWater = 0;
+        int leftPointer = 0;
+        int rightPointer = landscape.length - 1;
+
+        for (int height = 1; height <= maxHeight; height++) {
+            while (landscape[leftPointer] < height) {
+                leftPointer += 1;
+            }
+
+            while (landscape[rightPointer] < height) {
+                rightPointer -= 1;
+            }
+
+            totalWater += (rightPointer - leftPointer + 1);
+        }
+        totalWater -= numberOfBlocks;
+
+        return totalWater;
+    }
+
+
+//    // O(N), O(N)
+//    public long calculateWaterAmount(int[] landscape) {
+//        if (!validateCorrectness(landscape)) {
+//            return 0;
+//        }
+//
+//        int answer = 0;
+//        int size = landscape.length;
+//
+//        int[] leftMax = new int[size];
+//        int[] rightMax = new int[size];
+//
+//        leftMax[0] = landscape[0];
+//        for (int column = 1; column < size; column++) {
+//            leftMax[column] = Math.max(landscape[column], leftMax[column - 1]);
+//        }
+//
+//        rightMax[size - 1] = landscape[size - 1];
+//        for (int column = size - 2; column >= 0; column--) {
+//            rightMax[column] = Math.max(landscape[column], rightMax[column + 1]);
+//        }
+//
+//        for (int column = 1; column < size - 1; column++) {
+//            answer += Math.min(leftMax[column], rightMax[column]) - landscape[column];
+//        }
+//
+//        return answer;
+//    }
 
     public static void main(String[] args) {
         WaterCalculator waterCalculator = new WaterCalculator();
